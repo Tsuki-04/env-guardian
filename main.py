2a58f390b20585f5a2cf28772f86c687fb7d3ac2
Tsuki <alba.vgk.2005@gmail.com>
2026-04-12 21:28:07 +0200

    Add APP_KEY validation rule

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from src.analyzer import parse_env_file
-from rules.security import check_app_debug
+from rules.security import check_app_debug, check_app_key
 
 env_vars = parse_env_file(".env.example")
 
 results = []
 results.extend(check_app_debug(env_vars))
+results.extend(check_app_key(env_vars))
 
 for result in results:
     print(result)
--- rules/security.py
+++ rules/security.py
@@ -7,3 +7,17 @@ def check_app_debug(env_vars):
         results.append("[WARNING] APP_DEBUG is enabled. Disable it in production.")
 
     return results
+    
+def check_app_key(env_vars):
+    results = []
+
+    if "APP_KEY" not in env_vars:
+        results.append("[ERROR] APP_KEY is missing.")
+        return results
+
+    app_key = env_vars.get("APP_KEY", "").strip()
+
+    if app_key == "":
+        results.append("[ERROR] APP_KEY is empty.")
+
+    return results
